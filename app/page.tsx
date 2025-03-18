b42b8a89ee1867a7ddcf9f05967236bf6c80e665
Hafiz2023 <adilamin374@gmail.com>
2025-03-18 17:48:08 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
+"ue client";
 import NetworkLanding from "./NetworkLanding/page";
 import MiningSection from "./MiningSection/page";
-
 import MiningSectionTwo from "./MiningSectionTwo/page";
-import FooterOne from "./Footer/page";
 import Footer from "./Footer/page";
 
 export default function Home() {
